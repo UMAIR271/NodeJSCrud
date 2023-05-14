4dd1fd109afdc2768a59655fb53f9715bb378c3a
UMAIR271 <malikumair2701@gmail.com>
2023-05-14 23:59:43 +0500

    registeration code

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,11 +1,13 @@
 import User from "../models/user.schema.js";
 import asyncHandler from "../services/asyncHandler.js";
 import CustomError from "../services/CustomError.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 export const createUser = asyncHandler(async (req, res) => {
-  const { name, email } = req.body;
+  const { firstName, lastName, userName, email, password, gender } = req.body;
 
-  if (!name || !email) {
+  if (!userName || !email) {
     throw new CustomError("Please enter name and email", 400);
   }
 
@@ -13,6 +15,11 @@ export const createUser = asyncHandler(async (req, res) => {
   if (existingUser) {
     throw new CustomError("Email already exist", 404);
   }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+  
+  const newUser = new User({ email, password: hashedPassword });
+  newUser.save();
 
   const user = await User.create({ name, email });
   res.status(200).json({
--- src/models/user.schema.js
+++ src/models/user.schema.js
@@ -2,14 +2,32 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
   {
-    name: {
+    FisrtName: {
       type: String,
-      required: [true, "name is required"],
+      required: [true, "FirstName is required"],
+      maxLenght: [50, "name must be less then 50 char"],
+    },
+    LastName: {
+      type: String,
+      required: [true, "LastName is required"],
+      maxLenght: [50, "name must be less then 50 char"],
+    },
+    UserName: {
+      type: String,
+      required: [true, "user name is required"],
       maxLenght: [50, "name must be less then 50 char"],
     },
     email: {
       type: String,
       required: [true, "email is required"],
+    },
+    password: {
+      type: String,
+      required: [true, "password is required"],
+    },
+    Gender: {
+      type: String,
+      required: [true, "Gender is required"],
     },
   },
   { timestamps: true }
