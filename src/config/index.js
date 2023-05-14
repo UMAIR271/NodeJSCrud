337dc1af3f2cbd27c2d14e1020e51652a3f66b6e
Manish Dash Sharma <60460877+manishdashsharma@users.noreply.github.com>
2023-05-15 01:04:52 +0530

    removed mongo url

--- src/config/index.js
+++ src/config/index.js
@@ -5,8 +5,7 @@ dotenv.config();
 const config = {
   PORT: process.env.PORT || 5000,
   MONGODB_URL:
-    process.env.MONGODB_URL ||
-    "mongodb+srv://admin:admin@cluster0.shfhh1b.mongodb.net/CRUD?retryWrites=true&w=majority",
+    process.env.MONGODB_URL ,
 };
 
 export default config;
