0466800909037552cfa60e08efb2dd31c9472d59
UMAIR271 <malikumair2701@gmail.com>
2023-05-16 00:39:46 +0500

    get to post by issueId

--- src/routes/issue.router.js
+++ src/routes/issue.router.js
@@ -9,7 +9,7 @@ const router = Router();
 
 router.post("/issue-create", createIssue);
 router.get("/allIssueData", getAllIssue);
-router.get("/getIssueById", getIssueById);
+router.post("/getIssueById", getIssueById);
 router.post("/deleteIsuue", deleteIssue);
 router.put("/updateIssue/:id", deleteIssue);
 export default router;
